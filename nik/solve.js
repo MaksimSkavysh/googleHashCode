b991392e1ce08079694cedca1a07f48d23e825e7
liknens <nik.nikitenok@gmail.com>
2020-02-20 22:03:58 +0300

    firt submit

--- nik/solve.js
+++ nik/solve.js
@@ -6,19 +6,84 @@ const store = require('./store.js')
 const INPUTS = ['a_example', 'b_read_on', 'c_incunabula', 'd_tough_choices', 'e_so_many_books', 'f_libraries_of_the_world']
 
 
-const findMax = (data) => {
-  let maxVal = 0
-  let maxLibId
+const NAME = INPUTS[0]
+
+
+
+
+
+
+
+const fitLib = (data, lib, ts) => {
+  const profitableTs = ts - lib.signup
+  const booksLimit = profitableTs * lib.parallel
+  let score = 0
+  lib.bookIds.sort((a, b) => data.bookScore[b] - data.bookScore[a])
+  const usedIds = []
+  for (let i = 0; i < booksLimit && i < lib.bookIds.length; i++) {
+    const bookId = lib.bookIds[i]
+    score += data.bookScore[bookId]
+    usedIds.push(bookId)
+  }
+  return {
+    score,
+    usedIds,
+  }
 }
 
+const findMax = (data, ts) => {
+  let maxVal = { score: 0 }
+  let maxLib = null
 
-const NAME = INPUTS[2]
+  for(const lib of data.libs) {
+    const fit = fitLib(data, lib, ts)
+    if (fit.score > maxVal.score) {
+      maxVal = fit
+      maxLib = lib
+    }
+  }
+
+
+  return {
+    lib: maxLib,
+    usedIds: maxVal.usedIds,
+    score: maxVal.score
+  }
+}
+
 
 const solve = (inputName) => {
   const filename = inputName
   const data = parseInput(filename)
-  const
+
+  const result = []
+
+  let ts = data.days
+  let maxLib
+  do {
+    maxLib = findMax(data, ts)
+    if (!maxLib.lib) {
+      break
+    }
+    // console.log(maxLib)
+    maxLib.usedIds.forEach(id => {
+      data.bookScore[id] = 0
+    })
+    result.push(`${maxLib.lib.id} ${maxLib.usedIds.length}\n${maxLib.usedIds.join(' ')}`)
+    ts -= maxLib.lib.signup
+    maxLib.lib.signup = ts + 10000000
+  } while(ts > 0)
+
+  const resultStr = `${result.length}\n${result.join('\n')}`
+
+  fs.writeFileSync('../out/' + inputName + '.txt', resultStr)
+  //console.log(resultStr)
 }
 
 
-solve(NAME)
+
+// INPUTS.forEach(name => solve(name))
+
+solve(INPUTS[4])
+
+
