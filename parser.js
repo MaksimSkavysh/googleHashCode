d41f773b2dd9ccd20025d587df4ef6b54073fcea
liknens <nik.nikitenok@gmail.com>
2021-02-25 21:08:48 +0300

    parser

---
+++ parser.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const _ = require('lodash')
+
+const intReg = /^\d+$/
+
+const pi = a => intReg.test(a) ? parseInt(a, 10) : a
+
+const createReader = filename => {
+  const fileLines = fs.readFileSync(filename).toString().split('\n')
+  let index = 0
+  return () => fileLines[index++].split(' ').map(pi)
+}
+
+
+const parseInput = filename => {
+  const ril = createReader('./inputs/' + filename + '.txt')
+  const [TIME, intersections, STREETS_COUNT, CARS_COUNT, points] = ril()
+  const streets = []
+  for (let i = 0; i < STREETS_COUNT; i++) {
+    const [from, to, name, time] = ril()
+    streets.push({
+      from,
+      to,
+      name,
+      time,
+    })
+  }
+  const cars = []
+
+  for (let i = 0; i < CARS_COUNT; i++) {
+    const carPath = ril()
+    carPath.shift()
+    cars.push(carPath)
+  }
+
+  const res = {
+    TIME,
+    intersections,
+    streets,
+    cars,
+    points,
+  }
+  return res
+
+}
+
+console.log(parseInput('a'))
